eca66463595a4281297f94c035e769cae8343ac4
Raj sinha <94060322+r-a-j-s-i-n-h-a@users.noreply.github.com>
2022-12-22 23:46:55 +0530

    Create Integer to roman.java

---
+++ Integer to roman.java
@@ -0,0 +1,16 @@
+class Solution {
+  public String intToRoman(int num) {
+        int arabs[] = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+        String[] romans = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+        StringBuilder sb = new StringBuilder();
+        
+        for (int i = 0; i < arabs.length; i++) {
+            while (num >= arabs[i]) {
+                sb.append(romans[i]);
+                num -= arabs[i];
+            }
+        }
+        
+        return sb.toString();
+    }
+}
