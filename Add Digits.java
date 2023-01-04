d983f3b438c1a83f9194be0d9b557b6ea68048f5
Raj sinha <94060322+r-a-j-s-i-n-h-a@users.noreply.github.com>
2023-01-04 23:51:39 +0530

    Create Add Digits.java

---
+++ Add Digits.java
@@ -0,0 +1,13 @@
+import java.lang.Math;
+
+class Solution {
+    public int addDigits(int num) {
+        if( num % 9 == 0 && num != 0)
+        return 9;
+
+        if( num > 9)
+        return num % 9;
+
+        return num;
+    }
+}
