a2a8cd40f2e912286ccb0a6525c1414098e46519
Raj sinha <94060322+r-a-j-s-i-n-h-a@users.noreply.github.com>
2023-04-02 02:03:49 +0530

    Time: 0 ms (100.00%), Space: 39.5 MB (49.75%) - LeetHub

---
+++ 0461-hamming-distance/0461-hamming-distance.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int hammingDistance(int x, int y) 
+    {
+        int num = 0;
+
+        while (x > 0 || y > 0)
+        {
+            if (x % 2 != y % 2) num++;
+            x /= 2;
+            y /= 2;
+        }    
+
+        return num;
+    }
+}
