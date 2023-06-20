b4f866e6d7711014347e9533c5439d7b5b620426
Raj sinha <94060322+r-a-j-s-i-n-h-a@users.noreply.github.com>
2023-06-21 00:12:17 +0530

    Time: 0 ms (100.00%), Space: 40.6 MB (99.82%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
@@ -1,33 +1,14 @@
 class Solution {
-      public boolean searchMatrix(int[][] matrix, int target) 
-    {
-        int row=matrix.length;
-        int col=matrix[0].length;
-        
-       for(int i=0; i<row ; i++)
-       {
-           if(target>=matrix[i][0] && target <=matrix[i][col-1])
-           {
-               if(binarysearch(matrix[i],0,col-1,target))//binary search for finding in perticular row
-                    return true;
-           }
-       }
-        
-        return false;    
-    }
-
-    public boolean binarysearch(int[] nums,int s,int e,int target) //binary search function
-    {
-        if(s<=e)
-        {
-            int mid=(s+e)/2;
-            if(nums[mid]==target)
-                return true;
-            else if(target>nums[mid])
-                return binarysearch(nums,mid+1,e,target);
-            else
-                return binarysearch(nums,s,mid-1,target);            
-        }
-        return false;
+    public boolean searchMatrix(int[][] matrix, int target) {
+      int i = 0, j = matrix[0].length - 1;
+      while(i < matrix.length && j >= 0) {
+        if(matrix[i][j] == target)
+          return true;
+        else if(matrix[i][j] > target)
+          j --;
+        else if(matrix[i][j] < target)
+          i ++;
+      }
+      return false;
     }
 }
